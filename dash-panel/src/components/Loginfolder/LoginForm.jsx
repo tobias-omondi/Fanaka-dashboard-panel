238eef50e9e08f1aed82203c0c10bc32d80684b2
tobias <tobias.omondi@student.moringaschool.com>
2025-02-26 11:18:20 +0300

    intergrated admin login form html

--- dash-panel/src/components/Loginfolder/LoginForm.jsx
+++ dash-panel/src/components/Loginfolder/LoginForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const LoginForm = () => {
   return (
     <div>
-     <div className='bg-white flex flex-col justify-center items-center w-xl mx-auto shadow-lg rounded-4xl p-10'>
+     <div className='bg-white flex flex-col justify-center items-center w-80 md:xl mx-auto shadow-lg rounded-4xl p-10'>
        <h2 className='text-gray-500 font-bold text-center'>Admin Login</h2>
        <div className='flex flex-col'>
         <form className='flex flex-col gap-2'>
@@ -11,7 +11,7 @@ const LoginForm = () => {
             <input type='name' id='name'
             className="bg-white border border-gray-900 p-1.5 text-sm rounded block focus:outline-none focus:border-orange-500 focus:ring-1 focus:ring-orange-500"/>
             <label className='text-gray-600'>Password:</label>
-            <input type='password' id='name' 
+            <input type='password' id='password' 
             className= "bg-white border border-gray-900 p-1.5 text-sm rounded block focus:outline-none focus:border-orange-500 focus:ring-1 focus:ring-orange-500"/>
             <div className='justify-center items-center flex mx-auto '>
              <button type='submit'
